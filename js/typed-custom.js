1cc28052b1df18cfd587e821523a17d3aabed8fe
haojx <1090468413@qq.com>
2021-07-09 16:46:10 +0800

    1

---
+++ js/typed-custom.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){$(function(){$(".typed").typed({stringsElement:$(".typed-strings"),typeSpeed:200,backDelay:1500,loop:true,contentType:"html",loopCount:false,callback:function(){null},resetCallback:function(){newTyped()}})})});
